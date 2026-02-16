c189ee521f8341bf109ff18884981746c350a0bc
rahatalmas <rahat.almas5599@gmail.com>
2026-02-16 08:25:55 +0600

    addr

--- cmd/main.go
+++ cmd/main.go
@@ -16,18 +16,18 @@ func main() {
 	go h.Run()
 
 	// Setup routes
-    //ws://localhost/customer
+	//ws://localhost/customer
 	http.HandleFunc("/customer", func(w http.ResponseWriter, r *http.Request) {
 		handler.CustomerHandler(h, w, r)
 	})
-    //ws://localhost/customer
+	//ws://localhost/customer
 
 	http.HandleFunc("/human-agent", func(w http.ResponseWriter, r *http.Request) {
 		handler.HumanAgentHandler(h, w, r)
 	})
 	//
 	// Start server
-	addr := ":4646"
+	addr := "0.0.0.0:4646"
 	fmt.Printf("Server listening on %s\n", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal("ListenAndServe error: ", err)
